d343eab94d31e7d0cd77d6c12f33b00e5bb790b7
mdmamunurrashidhridoy <rashid15-5273@diu.eud.bd>
2025-06-19 14:33:59 +0600

    heelo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel='stylesheet' href='{{asset('asset/css/style.css') }}'>
 </head>
 <body>
-    <h1>
-        ha bhai choltesi , kire bhai
-    </h1>
+    <nav>
+        <div><h6>Hriody</h6></div>
+        <div>
+            <a href="">Home</a>
+            <a href="">Education</a>
+            <a href="">Skills</a>
+            <a href="">My Story</a>
+            <a href="">Contacts</a>
+        </div>
+    </nav>
+<h1>Hey, I am Hridoy🙋‍♂️,</h1>
+    <img src="{{ asset('asset/image/image.png') }}" alt="Photo of X">
+    <div class="info">
+      <h2>About Me</h2>
+      <p>I’m a passionate student, currently learning:</p>
+      <ul>
+        <li>Computer Science & Engineering</li>
+        <li>Political Science</li>
+        <li>Communication Science</li>
+      </ul>
+      <p>Let's connect and create something great together!</p>
+    </div>
+    
 </body>
 </html>
